2c40b96724b0342c005259ca87dde4c3e7461c6a
Pradeep Kumar Singh <94063026+PKumar9389@users.noreply.github.com>
2022-02-01 17:53:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// left side
+const image = document.getElementById("move");
+const images = document.querySelectorAll("img");
+
+let start = 0;
+function move(){
+    start++;
+    if(start>images.length-1){
+        start = 0;
+    }
+    image.style.transform=`translateX(${-start*300}px)`;
+}
+setInterval(move, 500);
+
+// middle one
+const image2 = document.getElementById("move2");
+const images2 = document.querySelectorAll("img");
+
+let start2=0;
+function move2(){
+    start2++;
+    if(start2>images2.length-1){
+        start2=0;
+    }
+    image2.style.transform=`translateY(${-start2*470}px)`;
+}
+setInterval(move2, 500);
+
+// right side
+const image1 = document.getElementById("move1");
+const images1 = document.querySelectorAll("img");
+
+let start1 = 0;
+function move1(){
+    start1++;
+    if(start1>images1.length-1){
+        start1 = 0;
+    }
+    image1.style.transform=`translateX(${-start1*300}px)`;
+}
+setInterval(move1, 500);
+
+// stars
+function stars(){
+    const stars = document.createElement("div");
+    stars.classList.add("stars");
+    stars.style.left=Math.random()*100+"vw";
+    stars.innerText= "🌟";
+    document.body.appendChild(stars);
+    setTimeout(() =>{
+        stars.remove();
+    }, 3000);
+}
+
+setInterval(stars,70);
